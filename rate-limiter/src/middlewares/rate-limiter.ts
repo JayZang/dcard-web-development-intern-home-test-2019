f96907bfdf421069c68d4eb6282084e1ce10c552
JayZang <sa841218as@gmail.com>
2019-05-15 15:49:18 +0800

    rate-limiter/解決 race condition

--- rate-limiter/src/middlewares/rate-limiter.ts
+++ rate-limiter/src/middlewares/rate-limiter.ts
@@ -4,8 +4,10 @@ import rateLimiterConfig from "../config/redis-rate-limiter";
 
 const redisHost = rateLimiterConfig.redisHost;
 const redisPort = rateLimiterConfig.redisPort;
-const duration = rateLimiterConfig.duration;
+const duration  = rateLimiterConfig.duration;
 const limitCountPerDuration = rateLimiterConfig.limitCountPerDuration;
+
+// 常駐之 redis client，用於監控 redis server 是否還活著
 const client = redis.createClient(redisPort, redisHost);
 
 client.on("connect", () => {
@@ -18,78 +20,106 @@ client.on("error", (err) => {
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip;
-    const data = await getRedisDataByIp(ip);
-    const resetDate = new Date(data.resetDate).toString();
+    const hashKey = `hash-key-${ip}`;
+    const workFlow = () => {
+        // 建立一個新的 redis client 端
+        // 才能使用 watch 來監控不同 client 是否有針對同一筆 key 資料做修改
+        const client = redis.createClient(redisPort, redisHost);
 
-    // 判斷 Request 次數是否已經大於等於每一時間區間內之最大數量
-    // 不用考慮 reset 時間是因為已在 Redis 做設置，超過 resetDate 會自動清除該筆資料
-    if (data.count >= limitCountPerDuration) {
-        res.setHeader("X-RateLimit-Remaining", 0);
-        res.setHeader("X-RateLimit-Reset", resetDate);
-        res.status(429).send("Too Many Requests");
-        return;
-    }
+        client.on("error", () => client.quit());
 
-    // Request 次數加 1
-    data.count++;
+        // Redis Watch 只對一般 key 有用，若為 hash 的話沒有效
+        // 因此多加使用 key 來做為類似鎖的功用，用以判斷該值是否有被其他 client 修改
+        client.watch(ip, async (err) => {
+            if (err) {
+                console.log("watch error");
+                console.log(err.toString());
+                client.unwatch();
+                return res.status(500).send();
+            }
 
-    // 資料更新回 Redis Server
-    setRedisDataByIp(ip, data);
+            // 向 redis server 取得資料
+            const data = await getRedisDataByHash(hashKey);
+            const resetDate = new Date(data.resetDate).toString();
 
-    // 計算剩餘次數
-    const remainingCount = limitCountPerDuration - data.count;
+            // 判斷有無超過每單位時間之最大 request 數量
+            if (data.count >= limitCountPerDuration) {
+                res.setHeader("X-RateLimit-Remaining", 0);
+                res.setHeader("X-RateLimit-Reset", resetDate);
+                res.status(429).send("Too Many Requests");
+                client.unwatch();
+                return;
+            }
 
-    res.setHeader("X-RateLimit-Remaining", remainingCount);
-    res.setHeader("X-RateLimit-Reset", resetDate);
+            data.count++;
 
-    next();
+            // 使用 redis multi
+            client.multi()
+                .set(ip, data.count.toString())
+                .hset(hashKey, "count", data.count.toString())
+                .hset(hashKey, "resetDate", data.resetDate.toString())
+                .expireat(ip, Math.floor(data.resetDate / 1000))
+                .expireat(hashKey, Math.floor(data.resetDate / 1000))
+                .exec((err, reply) => {
+                    console.log(reply);
+
+                    // handle redis server error
+                    if (err) {
+                        console.log("exec error");
+                        console.log(err.toString());
+                        return res.status(500).send();
+                    }
+
+                    // 若 reply === null 代表其他 client 修改過對應 key 之 value
+                    // 因此此次執行不成功
+                    // 則重新執行工作取得最新資料再存回 server
+                    else if (!reply) {
+                        console.log("conflict");
+                        setTimeout(workFlow, 1000);
+                    }
+
+                    // 儲存成功，設置 http header 及 斷開此 client 連線
+                    else {
+                        const remainingCount = limitCountPerDuration - data.count;
+
+                        res.setHeader("X-RateLimit-Remaining", remainingCount);
+                        res.setHeader("X-RateLimit-Reset", resetDate);
+
+                        client.quit(next);
+                    }
+                });
+        });
+    };
+
+    workFlow();
 };
 
 /**
- * Redis key(IP) 對應之資料
+ * Redis Hash 對應之所有資料
  */
-interface RateLimitDictionaryData {
-    ip: string;
+interface RateLimitHashData {
     count: number;
     resetDate: number;
 }
 
 /**
- * 向 Redis Server 取得對應 IP 之資料
+ * 向 Redis Server 取得對應 hash 之資料
  */
-function getRedisDataByIp(ip: string): Promise<RateLimitDictionaryData> {
+function getRedisDataByHash(hash: string): Promise<RateLimitHashData> {
     return new Promise((resolve, reject) => {
-        client.hgetall(ip, (err, reply) => {
+        // 注意: 此 client 為常駐之 redis client
+        client.hgetall(hash, (err, reply) => {
             if (err) {
                 reject(err);
                 return;
             }
 
-            const data: RateLimitDictionaryData = {
-                ip,
+            const data: RateLimitHashData = {
                 count: reply ? parseInt(reply.count) : 0,
                 resetDate: reply ? parseInt(reply.resetDate) : (new Date().valueOf() + duration),
             };
 
             resolve(data);
         });
-    });
-}
-
-/**
- * 向 Redis Server 設定對應 IP 之資料
- */
-function setRedisDataByIp(ip: string, data: RateLimitDictionaryData) {
-    return new Promise((resolve, reject) => {
-        const resetDate = data.resetDate;
-
-        client.hset(ip, "ip", data.ip);
-        client.hset(ip, "count", data.count.toString());
-        client.hset(ip, "resetDate", resetDate.toString());
-
-        // resetDate 除以 1000 是因為該參數不考慮毫秒
-        client.expireat(ip, Math.floor(resetDate / 1000));
-
-        resolve();
     });
 }
